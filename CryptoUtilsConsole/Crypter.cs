1f3374892310bb5a6b3b719598af0abd3a4b2a5a
melardev <melardev@gmail.com>
2017-05-14 08:58:13 +0200

    Add project files.

---
+++ CryptoUtilsConsole/Crypter.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoUtilsConsole
+{
+    class Crypter
+    {
+        public enum Mode
+        {
+            ENCRYPT,
+            DECRYPT
+        };
+
+        public static string getMD5(string message)
+        {
+            using (var md5 = MD5.Create())
+            {
+                byte[] hashmeHashed = md5.ComputeHash(Encoding.UTF8.GetBytes(message));
+                string result = BitConverter.ToString(hashmeHashed).Replace("-", "");
+                return (result);
+            }
+        }
+
+        public static string getSHA1(string message)
+        {
+            using (var sha1 = SHA1.Create())
+            {
+                byte[] hashmeHashed = sha1.ComputeHash(Encoding.UTF8.GetBytes(message));
+                string result = BitConverter.ToString(hashmeHashed).Replace("-", "");
+                return (result);
+            }
+        }
+
+        public static void getSHA(string message)
+        {
+            string hashme = "The quick brown fox jumps over the lazy dog";
+            SHA1 sha1 = SHA1.Create();
+            byte[] hashmeHashed = sha1.ComputeHash(Encoding.UTF8.GetBytes(hashme));
+            string result = BitConverter.ToString(hashmeHashed).Replace("-", "");
+            Console.WriteLine("SHA1 : " + result);
+
+
+            SHA256 sha256 = SHA256.Create();
+            hashmeHashed = sha256.ComputeHash(Encoding.UTF8.GetBytes(hashme));
+            result = BitConverter.ToString(hashmeHashed).Replace("-", "");
+            Console.WriteLine("SHA256 : " + result);
+
+            SHA512 sha512 = SHA512.Create();
+            hashmeHashed = sha512.ComputeHash(Encoding.UTF8.GetBytes(hashme));
+            result = BitConverter.ToString(hashmeHashed).Replace("-", "");
+            Console.WriteLine("SHA512 : " + result);
+        }
+
+        public static void getHMACMD5(string message)
+        {
+            using (var randonNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                byte[] key = new byte[32];
+                randonNumberGenerator.GetBytes(key);
+                using (var hmacmd5 = new HMACMD5(key))
+                {
+                    byte[] hashmeHashed = hmacmd5.ComputeHash(Encoding.UTF8.GetBytes(message));
+                    string result = BitConverter.ToString(hashmeHashed).Replace("-", "");
+                    Console.WriteLine("MD5 : " + result);
+                }
+            }
+        }
+
+        public static void getHMACSHA1(string message)
+        {
+            using (var randonNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                byte[] key = new byte[32];
+                randonNumberGenerator.GetBytes(key);
+                Console.WriteLine("HMAC : " + BitConverter.ToString(key).Replace("-", ""));
+                using (var hmacsha1 = new HMACSHA1(key))
+                {
+                    byte[] hashmeHashed = hmacsha1.ComputeHash(Encoding.UTF8.GetBytes(message));
+                    string result = BitConverter.ToString(hashmeHashed).Replace("-", "");
+                    Console.WriteLine("SHA1 : " + result);
+                }
+
+
+            }
+        }
+
+        static byte[] DESCrypto(Mode mode, byte[] IV, byte[] key, byte[] message)
+        {
+
+            using (var DES = new DESCryptoServiceProvider())
+            {
+                DES.IV = IV;
+                DES.Key = key;
+                DES.Mode = CipherMode.CBC;
+                DES.Padding = PaddingMode.PKCS7;
+
+
+                using (var memStream = new MemoryStream())
+                {
+                    CryptoStream cryptoStream = null;
+
+                    if (mode == Mode.ENCRYPT)
+                        cryptoStream = new CryptoStream(memStream, DES.CreateEncryptor(), CryptoStreamMode.Write);
+                    else if (mode == Mode.DECRYPT)
+                        cryptoStream = new CryptoStream(memStream, DES.CreateDecryptor(), CryptoStreamMode.Write);
+
+                    if (cryptoStream == null)
+                        return null;
+
+                    cryptoStream.Write(message, 0, message.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memStream.ToArray();
+                }
+            }
+        }
+
+
+
+        static byte[] AESCrypto(Mode mode, AesCryptoServiceProvider aes, byte[] message)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                CryptoStream cryptoStream = null;
+
+                if (mode == Mode.ENCRYPT)
+                    cryptoStream = new CryptoStream(memStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+                else if (mode == Mode.DECRYPT)
+                    cryptoStream = new CryptoStream(memStream, aes.CreateDecryptor(), CryptoStreamMode.Write);
+
+                if (cryptoStream == null)
+                    return null;
+
+                cryptoStream.Write(message, 0, message.Length);
+                cryptoStream.FlushFinalBlock();
+                return memStream.ToArray();
+            }
+        }
+
+        static byte[] TripleDESCrypto(Mode mode, byte[] IV, byte[] key, byte[] message)
+        {
+            using (var tripleDes = new TripleDESCryptoServiceProvider())
+            {
+                tripleDes.IV = IV;
+                tripleDes.Key = key;
+                tripleDes.Mode = CipherMode.CBC;
+                tripleDes.Padding = PaddingMode.PKCS7;
+
+
+                using (var memStream = new MemoryStream())
+                {
+                    CryptoStream cryptoStream = null;
+
+                    if (mode == Mode.ENCRYPT)
+                        cryptoStream = new CryptoStream(memStream, tripleDes.CreateEncryptor(), CryptoStreamMode.Write);
+                    else if (mode == Mode.DECRYPT)
+                        cryptoStream = new CryptoStream(memStream, tripleDes.CreateDecryptor(), CryptoStreamMode.Write);
+
+                    if (cryptoStream == null)
+                        return null;
+
+                    cryptoStream.Write(message, 0, message.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memStream.ToArray();
+                }
+            }
+        }
+
+        static void test()
+        {
+            string message = "The quick brown fox jumps over the lazy dog";
+            var random = new Random();
+            byte[] IV = new byte[8];
+            random.NextBytes(IV);
+            byte[] key = new byte[8];
+            random.NextBytes(key);
+            byte[] encrypted = DESCrypto(Crypter.Mode.ENCRYPT, IV, key, Encoding.UTF8.GetBytes(message));
+            Console.WriteLine("Encrypted Text :" + BitConverter.ToString(encrypted).Replace("-", ""));
+            byte[] decrypted = DESCrypto(Crypter.Mode.DECRYPT, IV, key, encrypted);
+            Console.WriteLine("Decrypted Text :" + Encoding.UTF8.GetString(decrypted));
+
+            bool baes = true;
+            if (baes)
+            {
+                //string message = "The quick brown fox jumps over the lazy dog";
+
+                using (var aes = new AesCryptoServiceProvider())
+                {
+                    aes.GenerateIV();
+                    aes.GenerateKey();
+                    aes.Mode = CipherMode.CBC;
+                    aes.Padding = PaddingMode.PKCS7;
+
+                    encrypted = AESCrypto(Mode.ENCRYPT, aes, Encoding.UTF8.GetBytes(message));
+                    Console.WriteLine("Encrypted Text :" + BitConverter.ToString(encrypted).Replace("-", ""));
+                    decrypted = AESCrypto(Mode.DECRYPT, aes, encrypted);
+                    Console.WriteLine("Decrypted Text :" + Encoding.UTF8.GetString(decrypted));
+                }
+
+                Console.ReadLine();
+            }
+            if (baes)
+            {
+                string filePath = "D:\\Users\\rabheus\\Desktop\\CPSC\\tutos\\sampletxt.txt";
+                string input;
+                string keyy = "youtubee";
+                while (true)
+                {
+                    Console.WriteLine("a) Encrypt");
+                    Console.WriteLine("b) Decrypt");
+                    Console.WriteLine("c) exit");
+
+                    input = Console.ReadLine();
+                    if (input == "c")
+                        break;
+                    else
+                    {
+                        if (input == "a")
+                            EncryptFile(filePath, keyy);
+                        else if (input == "b")
+                            DecryptFile(filePath, keyy);
+                    }
+                }
+            }
+        }
+
+        static void EncryptFile(string filePath, string key)
+        {
+            byte[] plainContent = File.ReadAllBytes(filePath);
+            using (var DES = new DESCryptoServiceProvider())
+            {
+                DES.IV = Encoding.UTF8.GetBytes(key);
+                DES.Key = Encoding.UTF8.GetBytes(key);
+                DES.Mode = CipherMode.CBC;
+                DES.Padding = PaddingMode.PKCS7;
+
+
+                using (var memStream = new MemoryStream())
+                {
+                    CryptoStream cryptoStream = new CryptoStream(memStream, DES.CreateEncryptor(),
+                        CryptoStreamMode.Write);
+
+                    cryptoStream.Write(plainContent, 0, plainContent.Length);
+                    cryptoStream.FlushFinalBlock();
+                    File.WriteAllBytes(filePath, memStream.ToArray());
+                    Console.WriteLine("Encrypted succesfully " + filePath);
+                }
+
+            }
+        }
+
+        private static void DecryptFile(string filePath, string key)
+        {
+            byte[] encrypted = File.ReadAllBytes(filePath);
+            using (var DES = new DESCryptoServiceProvider())
+            {
+                DES.IV = Encoding.UTF8.GetBytes(key);
+                DES.Key = Encoding.UTF8.GetBytes(key);
+                DES.Mode = CipherMode.CBC;
+                DES.Padding = PaddingMode.PKCS7;
+
+
+                using (var memStream = new MemoryStream())
+                {
+                    CryptoStream cryptoStream = new CryptoStream(memStream, DES.CreateDecryptor(),
+                        CryptoStreamMode.Write);
+
+                    cryptoStream.Write(encrypted, 0, encrypted.Length);
+                    cryptoStream.FlushFinalBlock();
+                    File.WriteAllBytes(filePath, memStream.ToArray());
+                    Console.WriteLine("Decrypted succesfully " + filePath);
+                }
+            }
+        }
+    }
+}
---
+++ CryptoUtilsConsole/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoUtilsConsole
+{
+    class Program
+    {
+
+        private static RSAParameters publicKey;
+        private static RSAParameters privateKey;
+        static string CONTAINER_NAME = "MyContainerName";
+        public enum KeySizes
+        {
+            SIZE_512 = 512,
+            SIZE_1024 = 1024,
+            SIZE_2048 = 2048,
+            SIZE_952 = 952,
+            SIZE_1369 = 1369
+        };
+        static void Main(string[] args)
+        {
+            string message = "The quick brown fox jumps over the lazy dog";
+            generateKeys();
+            byte[] encrypted = Encrypt(Encoding.UTF8.GetBytes(message));
+            byte[] decrypted = Decrypt(encrypted);
+            DeleteKeyInCSP();
+            Console.WriteLine("Original\n\t " + message + "\n");
+            Console.WriteLine("Encrypted\n\t" + BitConverter.ToString(encrypted).Replace("-", "") + "\n");
+            Console.WriteLine("Decrypted\n\t" + Encoding.UTF8.GetString(decrypted));
+            
+            Console.ReadLine();
+
+        }
+
+
+        static void generateKeys()
+        {
+            int rsa_provider = 1;
+
+            CspParameters cspParameters = new CspParameters(rsa_provider); //1 for rsa ; 13 for DSA ( Digital signature algorithm)
+            cspParameters.KeyContainerName = CONTAINER_NAME;
+            cspParameters.Flags = CspProviderFlags.UseMachineKeyStore;
+            cspParameters.ProviderName = "Microsoft Strong Cryptographic Provider";
+            var rsa = new RSACryptoServiceProvider(cspParameters);
+            rsa.PersistKeyInCsp = true;
+        }
+
+        public static void DeleteKeyInCSP()
+        {
+            var cspParams = new CspParameters();
+            cspParams.KeyContainerName = CONTAINER_NAME;
+            var rsa = new RSACryptoServiceProvider(cspParams);
+            rsa.PersistKeyInCsp = false;
+            rsa.Clear();
+        }
+
+
+        private static byte[] Encrypt(byte[] plain)
+        {
+            byte[] encrypted;
+            int rsa_provider = 1;
+            CspParameters cspParameters = new CspParameters(rsa_provider);
+            cspParameters.KeyContainerName = CONTAINER_NAME;
+
+            using (var rsa = new RSACryptoServiceProvider((int)KeySizes.SIZE_2048, cspParameters))
+            {
+                encrypted = rsa.Encrypt(plain, true);
+            }
+            return encrypted;
+        }
+
+        private static byte[] Decrypt(byte[] encrypted)
+        {
+            byte[] decrypted;
+
+            CspParameters cspParameters = new CspParameters();
+            cspParameters.KeyContainerName = CONTAINER_NAME;
+
+            using (var rsa = new RSACryptoServiceProvider((int)KeySizes.SIZE_2048, cspParameters))
+            {
+                decrypted = rsa.Decrypt(encrypted, true);
+            }
+            return decrypted;
+        }
+
+        /*
+        
+       private static void generateKeys(string publicKeyFile, string privateKeyFile)
+        {
+            using (var rsa = new RSACryptoServiceProvider((int)KeySizes.SIZE_2048))
+            {
+                rsa.PersistKeyInCsp = false;
+
+                if (File.Exists(privateKeyFile))
+                    File.Delete(privateKeyFile);
+
+                if (File.Exists(publicKeyFile))
+                    File.Delete(publicKeyFile);
+
+                string publicKey = rsa.ToXmlString(false);
+                File.WriteAllText(publicKeyFile, publicKey);
+                string privateKey = rsa.ToXmlString(true);
+                File.WriteAllText(privateKeyFile, privateKey);
+            }
+        }
+
+
+        private static byte[] Encrypt(string publicKeyFile, byte[] plain)
+        {
+            byte[] encrypted;
+            using (var rsa = new RSACryptoServiceProvider((int)KeySizes.SIZE_2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                string publicKey = File.ReadAllText(publicKeyFile);
+                rsa.FromXmlString(publicKey);
+                encrypted = rsa.Encrypt(plain, true);
+            }
+            return encrypted;
+        }
+
+        private static byte[] Decrypt(string privateKeyFile, byte[] encrypted)
+        {
+            byte[] decrypted;
+            using (var rsa = new RSACryptoServiceProvider((int)KeySizes.SIZE_2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                string privateKey = File.ReadAllText(privateKeyFile);
+                rsa.FromXmlString(privateKey);
+                decrypted = rsa.Decrypt(encrypted, true);
+            }
+            return decrypted;
+        }*/
+
+        /*
+      
+         static void generateKeys()
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false; //Don't store the keys in a key container
+                publicKey = rsa.ExportParameters(false);
+                privateKey = rsa.ExportParameters(true);
+            }
+        }
+
+        static byte[] Encrypt(byte[] input)
+        {
+            byte[] encrypted;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(publicKey);
+                encrypted = rsa.Encrypt(input, true);
+            }
+            return encrypted;
+        }
+
+        static byte[] Decrypt(byte[] input)
+        {
+            byte[] decrypted;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(privateKey);
+                decrypted = rsa.Decrypt(input, true);
+            }
+            return decrypted;
+        }
+
+        
+
+    */
+    }
+
+
+}
+
